38cfc4bf4dbc59ca322b0bd14dd6f1494e849539
Sander Boden <80114602+sanderboden@users.noreply.github.com>
2024-10-02 00:13:23 +0200

    init

---
+++ msparser/MSPdb.py
@@ -0,0 +1,135 @@
+from typing import List, Tuple, Generator
+from dataclasses import dataclass
+
+@dataclass
+class Record:
+    def __init__(
+            self,
+            name: str = None,
+            precursor_mz: float = None,
+            precursor_type: str = None,
+            smiles: str = None,
+            inchi: str = None,
+            formula: str = None,
+            retention_time: float = None,
+            ccs: float = None,
+            ionmode: str = None,
+            compound_class: str = None,
+            comment: str = None,
+            n_peaks: int = None,
+            peaks: List[Tuple[float, int]] = None,
+    ):
+        self.name = name
+        self.precursor_mz = precursor_mz
+        self.precursor_type = precursor_type
+        self.smiles = smiles
+        self.inchi = inchi
+        self.formula = formula
+        self.retention_time = retention_time
+        self.ccs = ccs
+        self.ionmode = ionmode
+        self.compound_class = compound_class
+        self.comment = comment
+        self.n_peaks = n_peaks
+        self.peaks = peaks if peaks else []
+    
+    def __str__(self):
+        return f'Record:\n{self.name}\n{self.precursor_mz}\n{self.precursor_type}\n{self.smiles}\n{self.inchi}\n{self.formula}\n{self.retention_time}\n{self.ccs}\n{self.ionmode}\n{self.compound_class}\n{self.comment}\n{self.n_peaks}\n{self.peaks}'
+
+
+class MSPdb:
+    def __init__(self):
+        self.records: List[Record] = []
+    
+    def load_file(self, filename: str):
+        f = open(filename, 'r')
+        items = f.read().strip().split('\n\n')
+        for item in items: # iterate over items in database 
+            rec = {}
+            peaks = []
+            for field in item.split('\n'): # iterate over fields in item
+                if ': ' in field: # fields are separated by ': '
+                    key, value = field.split(': ')
+                    key = key.strip()
+                    value = value.strip()
+                    rec[key] = value
+                        
+                elif '\t' in field: # peaks are separated by tab
+                    peaks.append(tuple(map(float, field.split('\t'))))
+
+                if 'CCS' not in rec: # some records do not have CCS
+                    rec['CCS'] = 'NA'
+
+            # create record from rec and peaks
+            try:
+                record = Record(
+                    name=rec.get('NAME'),
+                    precursor_mz=float(rec.get('PRECURSORMZ')),
+                    precursor_type=rec.get('PRECURSORTYPE'),
+                    smiles=rec.get('SMILES'),
+                    inchi=rec.get('INCHIKEY'),
+                    formula=rec.get('FORMULA'),
+                    retention_time=float(rec.get('RETENTIONTIME')),
+                    ccs=float(rec.get('CCS')) if not rec.get('CCS') == 'NA' else None,
+                    ionmode=rec.get('IONMODE'),
+                    compound_class=rec.get('COMPOUNDCLASS'),
+                    comment=rec.get('Comment'),
+                    n_peaks=int(rec.get('Num Peaks')),
+                    peaks=peaks,
+                )
+                self.records.append(record)
+                f.close()
+            except:
+                print('Record failed validation:', item.split('\n')) # if invalid, print record
+                f.close()
+        
+    def summary(self):
+        print(f"""
+        MSP Database Summary
+        --------------------------------
+        Number of records: {len(self.records)}
+        --------------------------------
+        Number of records with CCS: {len([record for record in self.records if record.ccs is not None])}
+        --------------------------------
+        Number of unique compound classes: {len(set([record.compound_class for record in self.records]))}
+        {'\t'.join(set([record.compound_class for record in self.records]))}
+        --------------------------------
+        Number of positive modes: {len([record for record in self.records if record.ionmode == 'Positive'])}
+        Number of negative modes: {len([record for record in self.records if record.ionmode == 'Negative'])}
+        --------------------------------
+        Number of precursor types: {len(set([record.precursor_type for record in self.records]))}
+        {'\t'.join(set([record.precursor_type for record in self.records]))}
+        --------------------------------
+                    """)
+    
+    def filter_class(self, compound_class: str, records: List[Record] = None) -> Generator[Record, None, None]:
+        for record in self.records if self.records else records:
+            if record.compound_class == compound_class:
+                yield record
+
+    def filter_ionmode(self, ionmode: str, records: List[Record] = None) -> Generator[Record, None, None]:
+        for record in self.records if self.records else records:
+            if record.ionmode == ionmode:
+                yield record
+
+    def ffilter_name(self, name: str, records: List[Record] = None) -> Generator[Record, None, None]:
+        for record in self.records if self.records else records:
+            if name in record.name:
+                yield record
+    
+    def filter_precursor_type(self, precursor_type: str, records: List[Record] = None) -> Generator[Record, None, None]:
+        for record in self.records if self.records else records:
+            if record.precursor_type == precursor_type:
+                yield record
+
+db = MSPdb()
+db.load_file('tmpdata/MSDIAL_plusPT_neg.msp')
+# db.load_file('tmpdata/MSDIAL_plusPT_pos.msp')
+
+db.summary()
+
+filterdb = MSPdb()
+filterdb.records = list(db.filter_class('PC'))
+filterdb.summary()
+filterdb.records = list(filterdb.filter_ionmode('Negative'))
+filterdb.summary()
